4bd3648fa7f6061ab2cdf361a93b99927570971d
wanderanimrod <wanderanimrod@gmail.com>
2013-10-19 18:20:08 +0300

    [nim] Removing unnecessary applications that are enabled by default

--- simple_list_view/simple_list_view/settings.py
+++ simple_list_view/simple_list_view/settings.py
@@ -116,8 +116,6 @@ TEMPLATE_DIRS = (
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
